eabead2f6ca29cee1d03bd13a2ff23e02751ed81
vinaybabu3962 <59788633+vinaybabu3962@users.noreply.github.com>
2020-09-20 13:39:06 +0530

    Add files via upload

---
+++ Web applications in PHP/WEEK5/index.php
@@ -0,0 +1,16 @@
+<title> VINAY BABU PHP</title>
+<h1>VINAY BABU PHP</h1>
+<pre>
+
+	*     *
+	 *   *
+	  * *
+	   *
+<p>
+<?php
+print hash('sha256', 'VINAY BABU');
+?>
+</p>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
